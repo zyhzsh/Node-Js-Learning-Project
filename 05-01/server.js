94f4e242a0bc412e30edfa1444f0214acf3c4432
Zhu,Shenghang S <s.zhu@student.fontys.nl>
2021-12-05 21:46:02 +0100

    import socket.io

--- 05-01/server.js
+++ 05-01/server.js
@@ -1,7 +1,32 @@
 var express = require('express')
+var bodyParser= require('body-parser')
 var app = express()
 
 app.use(express.static(__dirname));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
+
+var messages=[
+    {name:"Bob",message:"hi"},
+    {name:"Tom",message:"hey~!!"},
+]
+
+
+
+app.get('/messages',(req,res)=>{
+    res.send(messages);
+
+})
+
+
+
+app.post('/messages',(req,res)=>{
+    console.log(req.body);
+    messages.push(req.body);
+   res.sendStatus(200);
+})
+
+
 var server = app.listen(3000,()=>{
     console.log('server is listening on prot',server.address().port);
 })
