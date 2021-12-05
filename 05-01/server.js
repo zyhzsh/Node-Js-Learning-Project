96ac2e63f8f882790ec4020959bb76c986c20abe
Zhu,Shenghang S <s.zhu@student.fontys.nl>
2021-12-05 22:11:15 +0100

    working with socket.io

--- 05-01/server.js
+++ 05-01/server.js
@@ -1,6 +1,8 @@
 var express = require('express')
 var bodyParser= require('body-parser')
 var app = express()
+var http = require('http').Server(app)
+var io = require('socket.io')(http)
 
 app.use(express.static(__dirname));
 app.use(bodyParser.json());
@@ -22,11 +24,16 @@ app.get('/messages',(req,res)=>{
 
 app.post('/messages',(req,res)=>{
     console.log(req.body);
+    io.emit('message',req.body);
     messages.push(req.body);
    res.sendStatus(200);
 })
 
 
-var server = app.listen(3000,()=>{
+io.on('connection',(socket)=>{
+    console.log('a user connected ~! ');
+})
+
+var server = http.listen(3000,()=>{
     console.log('server is listening on prot',server.address().port);
 })
