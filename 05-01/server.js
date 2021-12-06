3f66e253588b7769cef6801fac16ea46bf46b0bd
Zhu,Shenghang S <s.zhu@student.fontys.nl>
2021-12-06 22:43:55 +0100

    link with mongodb

--- 05-01/server.js
+++ 05-01/server.js
@@ -1,39 +1,61 @@
 var express = require('express')
-var bodyParser= require('body-parser')
+var bodyParser = require('body-parser')
 var app = express()
 var http = require('http').Server(app)
 var io = require('socket.io')(http)
+var mongoose = require('mongoose')
+
+
+
+console.log(Object.keys(process.env));
+
+
+var username = 'shenghangzhu';
+var password = 'passwordpassword';
+
+const uri = `mongodb+srv://${username}:${password}@learnning.xztbj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+
+var Message = mongoose.model('Message', {
+    name: String,
+    message: String
+})
 
 app.use(express.static(__dirname));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:false}));
-
-var messages=[
-    {name:"Bob",message:"hi"},
-    {name:"Tom",message:"hey~!!"},
-]
+app.use(bodyParser.urlencoded({ extended: false }));
 
 
 
-app.get('/messages',(req,res)=>{
-    res.send(messages);
 
+app.get('/messages', (req, res) => {
+    Message.find({},(err,messages)=>{
+        res.send(messages);
+    })
 })
 
 
 
-app.post('/messages',(req,res)=>{
-    console.log(req.body);
-    io.emit('message',req.body);
-    messages.push(req.body);
-   res.sendStatus(200);
+app.post('/messages', (req, res) => {
+    var message = new Message(req.body);
+    message.save((err) => {
+        if (err) {
+            sendStatus(500)
+        }
+        io.emit('message', req.body);
+        res.sendStatus(200);
+    })
 })
 
 
-io.on('connection',(socket)=>{
+io.on('connection', (socket) => {
     console.log('a user connected ~! ');
 })
+mongoose.connect(uri, (err) => {
+    console.log('mongo db connection', err)
+})
 
-var server = http.listen(3000,()=>{
-    console.log('server is listening on prot',server.address().port);
+var server = http.listen(3000, () => {
+    console.log('server is listening on prot', server.address().port);
 })
